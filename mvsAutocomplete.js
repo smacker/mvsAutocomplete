b323f96f4ebf194a85735e541a6eae1dc18a93f3
Максим Сухарев <msukharev@ideco.ru>
2013-09-12 13:46:32 +0600

    now its work with ng-model

--- mvsAutocomplete.js
+++ mvsAutocomplete.js
@@ -31,38 +31,47 @@ angular.module('mvsAutocomplete', [])
     }
 
     return {
-      scope: {
-        model: '=',
-        source: '='
-      },
-      link: function(scope, element, attrs) {
+      restrict: 'EA',
+      scope: false,
+      require: '?ngModel',
+      replace: true,
+      template: '<input type="text">',
+      link: function(scope, element, attrs, ngModel) {
+        if(!ngModel) return;
+
         var valueGetter = attrs.valuePath && $parse(attrs.valuePath),
             labelGetter = attrs.labelPath && $parse(attrs.labelPath),
             modelGetter = (valueGetter && $parse('__obj')) || angular.identity,
             options = { // jQuery UI autocomplete options
               minLength: 0,
-              source: getSource(scope.source, valueGetter, labelGetter) || []
+              source: getSource(scope.$eval(attrs.source), valueGetter, labelGetter) || []
             };
 
+        ngModel.$render = function () {
+          element.val(ngModel.$viewValue);
+        };
+
+        ngModel.$formatters.push(function (value) {
+          if (value) {
+            return valueGetter(value);
+          }
+        });
+
+        // jQuery UI Autocomplete
         element.autocomplete(options);
 
         element.on('autocompleteselect', function (e, ui) {
-          // set model on select
-          scope.$apply(function() {
-            scope.model = modelGetter(ui.item);
-          });
+          ngModel.$setViewValue(modelGetter(ui.item));
         });
 
         element.bind('keyup', function () {
           // set model to null when input is empty
           if (!element.val().length) {
-            scope.$apply(function() {
-              scope.model = null;
-            });
+            ngModel.$setViewValue(null);
           }
         });
 
-        scope.$watch('source', function (source) {
+        scope.$watch(attrs.source, function (source) {
           // watch source changes
           element.autocomplete(
             'option', 'source', getSource(source, valueGetter, labelGetter)
